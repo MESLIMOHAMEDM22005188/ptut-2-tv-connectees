cebb19f2151bc7f1b21026bbce2fb0ffe65de54b
m22005188 <mohamed.mesli@etu.univ-amu.fr>
2024-02-16 14:22:50 +0100

    Chement des icons de méthéo

--- public/js/weather.js
+++ public/js/weather.js
@@ -1,6 +1,7 @@
 var meteoRequest = new XMLHttpRequest();
 var longitude = 5.4510;
 var latitude = 43.5156;
+var apiKey = 'dea48adba4f94388a0e155135241402'
 var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&lang=fr&APPID=ae546c64c1c36e47123b3d512efa723e";
 
 function refreshWeather() {
@@ -22,6 +23,7 @@ meteoRequest.onload = function () {
     document.getElementById('temperature').textContent = temp + "°C";
     document.getElementById('wind').textContent = vent + " KM/H - " + humidity + "% humidité";
     document.getElementById('sunsetTime').textContent = "Le soleil se couche à " + sunsetTime;
+    console.log(json.current.conditionText.icon)
 };
 
 function getTemp(json) {
@@ -41,5 +43,49 @@ function kelvinToC(kelvin) {
 function msToKmh(speed) {
     return speed * 3.6;
 }
+// mettre les icons
+function getIcon(weather){
+    switch (weather){
+        case 'Sunny':
+        case 'Clear':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/01d.svg';
+        case 'Partly cloudy':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/02d.svg';
+        case 'Cloudy':
+        case 'Overcast':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/03d.svg';
+        case 'Patchy light snow with thunder':
+        case 'Thundery outbreaks possible':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/04d.svg';
+        case 'Mist':
+        case 'Fog':
+        case 'Freezing fog':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/50d.svg';
+        case 'Patchy rain possible':
+        case 'Patchy light drizzle':
+        case 'Patchy light rain':
+        case 'Light rain':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/10d.svg';
+        case 'Patchy snow possible':
+        case 'Patchy sleet possible':
+        case 'Patchy freezing drizzle possible':
+        case 'Blowing snow':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/13d.svg';
+        case 'Moderate rain at times':
+        case 'Moderate rain':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/15d.svg';
+        case 'Heavy rain at times':
+        case 'Heavy rain':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/16d.svg';
+        case 'Light drizzle':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/14d.svg';
+        case 'Moderate or heavy rain with thunder':
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/17d.svg';
+        default:
+            return '/wp-content/plugins/ptut-2-tv-connectees/public/img/conditions/03d.svg';
 
+    }
+}
+
+refreshWeather();
 refreshWeather();
